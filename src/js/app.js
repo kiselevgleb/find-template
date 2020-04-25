ec1d4fe31264487b451dd68273d62d35c38e5811
kiselevgleb <kiselevgleb@gmail.com>
2020-04-25 23:03:40 +0300

    added fun find

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,13 @@
 // TODO: write your code here
 import sum from './basic';
 
-console.log('worked');
 
-console.log(sum([1, 2]));
+
+export default function find(a,b) {
+        return function(x) {
+            // console.log(x[a]);
+            // console.log(a);
+          return x[a] == b;
+        };
+      
+}
